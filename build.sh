659a3882b0be91888952d975e53ee943dc94f46c
br1an-S <br1an6pso@gmail.com>
2020-04-16 12:01:10 +0100

    Update build.sh
    Created build script for Mac to use for C++

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # This is the build script for Mac/Linux 
 mkdir build
 pushd build
-cmake -G "Visual Studio 12 2013" ..\
+cmake -G "Xcode" ..\
 cmake --build . --target INSTALL --config Release
 popd
 
